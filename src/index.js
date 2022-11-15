24c14dd8db387a10b92481bc615af647f834f3f9
kerry jalal <khariy34@hotmail.com>
2022-11-15 20:28:28 +0100

    in

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter  basename={process.env.PUBLIC_URL}  >
+    <HashRouter  >
       <ChakraProvider >
         <App />
       </ChakraProvider>
