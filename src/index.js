c6648dd3b79d308d8133abb2c6106c3c66bac38f
kerry jalal <khariy34@hotmail.com>
2022-11-20 13:21:04 +0100

    netfliy

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL} >
+    <HashRouter  >
       <ChakraProvider >
         <App />
       </ChakraProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
