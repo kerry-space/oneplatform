fefe55d24e082cae2eb5134b3ea12ae52ff964b8
kerry jalal <khariy34@hotmail.com>
2022-11-21 22:53:06 +0100

    update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,28 +3,22 @@ import "./style.scss";
 import teamBuild from "../images/engineering_team.svg";
 import teamImg from "../images/team_spirit.svg";
 
-
 const About = () => {
   return (
     <div className=" about">
-      <div className="about_text_title">
-        <h1 className="text-center">
+      <div className="about_text_title ">
+        <h1 className="text-center ">
           <strong>About us</strong>
         </h1>
-       <div className="container-fluid">
-        <div className="row">
-          <div className="col-12 px-0">
-          <img className="img-fluid" src={teamImg} alt="" />
-          </div>
 
+        <div className="container d-flex justify-content-center my-5">
+          <img className="img-fluid  rounded" src={teamImg} alt="" />
         </div>
-
-       </div>
       </div>
 
-      <div className="row about_container">
-        <div className="conatiner about_container_title col-sm ">
-          <h1>
+      <div className="container about_container my-5">
+        <div className="conatiner about_container_title  my-5">
+          <h1 className="my-3">
             <strong>who we are</strong>
           </h1>
           <p>
@@ -35,16 +29,19 @@ const About = () => {
             customers needs. With all in OnePlatform, helps you to reach more
             people by establishing your brand across every operating system,
             whether that be Android,ios,Pc, etc.
+            <br/>
+            <br/>
+            Oneplatform is different from must tech bussiness company out there, we are open to any  
+            suggesten relatet to bussines idea from customer, if you have an idea of the business and you’d like to start it but dont know how,
+            you might be looking for some direction. we can help you easily to launch a small business.
             <br />
             <br />
           </p>
         </div>
-        <div className="container about_container_img col-sm">
+        <div className="container  who-section ">
           <img className="img-fluid rounded" src={teamBuild} alt="" />
         </div>
       </div>
-
-
     </div>
   );
 };
